a54e8fccbc45145675f4c4628885ee4ae9e0e47a
Utsav Joshi <joshiutsav56@gmail.com>
2023-10-04 17:03:22 +0530

    04|10|2023 - updated the chat section more

--- client/src/pages/dashboard/Chats.js
+++ client/src/pages/dashboard/Chats.js
@@ -71,7 +71,7 @@ const ChatsElement = ({ id, img, name, msg, time, unread, online }) => {
 const Search = styled("div")(({ theme }) => ({
     position: "relative",
     borderRadius: 20,
-    backgroundColor: alpha(theme.palette.background.default, 1),
+    backgroundColor: alpha(theme.palette.background.paper, 1),
     marginRight: theme.spacing(2),
     marginLeft: 0,
     width: "100%",
