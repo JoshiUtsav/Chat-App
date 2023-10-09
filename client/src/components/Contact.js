ca7d2a24719df3cf5d3b679723d18f4f5cdb569b
Utsav Joshi <joshiutsav56@gmail.com>
2023-10-09 16:25:00 +0530

    09|10|2023 -- Contact Sidebar added

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,10 +1,116 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Avatar, Box, Button, Divider, IconButton, Stack, Typography } from '@mui/material';
+import { useTheme } from "@mui/material/styles"
+import { Bell, CaretRight, Phone, Prohibit, Star, Trash, VideoCamera, X } from 'phosphor-react';
+import { useDispatch } from 'react-redux';
+import { toggleSidebar } from '../redux/slices/app';
+import { faker } from '@faker-js/faker';
+import AntSwitch from "./AntSwitch"
 
 const Contact = () => {
+  const theme = useTheme();
+  const dispatch = useDispatch();
   return (
-    <Box sx={{ width: 320, }}>
-
+    <Box sx={{ width: 320, height: "100vh" }}>
+      <Stack sx={{ height: "100%" }}>
+        {/* Header */}
+        <Box sx={{
+          boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
+          width: "100%",
+          backgroundColor: theme.palette.mode === "light" ? "#f8FAff" : theme.palette.background
+        }}>
+          <Stack sx={{ height: "100%", p: 2 }} direction={"row"} alignItems={"center"} justifyContent={"space-between"} spacing={3}>
+            <Typography variant='subtitle2'>Contact info</Typography>
+            <IconButton onClick={() => {
+              dispatch(toggleSidebar())
+            }}>
+              <X />
+            </IconButton>
+          </Stack>
+        </Box>
+        {/* Body */}
+        <Stack sx={{ height: "100%", position: "relative", flexGrow: 1, overflowY: "scroll" }} p={3} spacing={3}>
+          <Stack alignItems={"center"} spacing={2} direction={"row"}>
+            <Avatar src={faker.image.avatar()} alt={faker.name.firstName()} sx={{ height: 64, width: 64 }} />
+            <Stack spacing={0.5}>
+              <Typography variant='article' fontWeight={600}>
+                {faker.name.fullName()}
+              </Typography>
+              <Typography variant='body2' fontWeight={500}>
+                {"+91 **********"}
+              </Typography>
+            </Stack>
+          </Stack>
+          <Stack direction={'row'} alignItems={"center"} justifyContent={"space-evenly"}>
+            <Stack spacing={1} alignItems={"center"}>
+              <IconButton>
+                <Phone />
+              </IconButton>
+              <Typography variant='overline'>Voice</Typography>
+            </Stack>
+            <Stack spacing={1} alignItems={"center"}>
+              <IconButton>
+                <VideoCamera />
+              </IconButton>
+              <Typography variant='overline'>Video</Typography>
+            </Stack>
+          </Stack>
+          <Divider />
+          <Stack spacing={0.5}>
+            <Typography variant='article'>About</Typography>
+            <Typography variant='body2'>Hey there I am using conversa</Typography>
+          </Stack>
+          <Divider />
+          <Stack direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
+            <Typography variant='subtitle2'>Media, Link & Docs</Typography>
+            <Button endIcon={<CaretRight />}>
+              200
+            </Button>
+          </Stack>
+          <Stack direction={"row"} spacing={2} alignItems={"center"}>
+            {[1, 2, 3].map((el) => (
+              <Box>
+                <img src={faker.image.animals()} alt={faker.name.fullName()} />
+              </Box>
+            ))}
+          </Stack>
+          <Divider />
+          <Stack direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
+            <Stack direction={'row'} alignItems={"center"} spacing={2}>
+              <Star size={21} />
+              <Typography variant='subtitle2'>Starred Message</Typography>
+            </Stack>
+            <IconButton>
+              <CaretRight />
+            </IconButton>
+          </Stack>
+          <Divider />
+          <Stack direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
+            <Stack direction={'row'} alignItems={"center"} spacing={2}>
+              <Bell size={21} />
+              <Typography variant='subtitle2'>Mute Notifications</Typography>
+            </Stack>
+            <AntSwitch />
+          </Stack>
+          <Divider />
+          <Typography>1 group in common</Typography>
+          <Stack direction={"row"} spacing={2} alignItems={"center"}>
+            <Avatar src={faker.image.avatar()} alt={faker.name.fullName()} />
+            <Stack spacing={0.5}>
+              <Typography variant='subtitle2'>Students</Typography>
+              <Typography variant='caption'>Owl, Parrot, Rabbit, You</Typography>
+            </Stack>
+          </Stack>
+          <Stack direction={"row"} alignItems={"center"} spacing={2}>
+            <Button startIcon={<Prohibit />} fullWidth variant='outlined'>
+              Block
+            </Button>
+            <Button startIcon={<Trash />} fullWidth variant='outlined'>
+              Delete
+            </Button>
+          </Stack>
+        </Stack>
+      </Stack>
     </Box>
   )
 }
